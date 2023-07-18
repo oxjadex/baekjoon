1889cf4ed95acea9f53a2e9492f32d461c2b730a
ìoxjadex <oharin765@gmail.com>
2023-07-18 23:30:35 +0900

    2557

---
+++ 1002.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () {
+    int x1, x2, y1, y2, r1, r2;
+    int x3, y3;
+    int n, i, j, c; 
+    scanf("%d", &n);
+    while (i--) {
+        scanf("%d %d %d %d %d %d", &x1, &y1, &r1, &r2, &x2, &y2);
+        //i제곱 더하기 y제곱==r1인 경우 
+    }
+}
